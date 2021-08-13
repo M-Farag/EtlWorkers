bd7336e4bb5c9b254dd4704272f6f017b616aaed
M-Farag <mina.farag@icloud.com>
2021-08-13 23:36:34 +0200

    UPD: Using the file name to simplify the process

--- EtlWorkers/workers/FileWorker.py
+++ EtlWorkers/workers/FileWorker.py
@@ -3,11 +3,8 @@ import uuid
 
 class FileWorker:
 
-    def __init__(self, file_path, file_name):
-        self._file_path = file_path
+    def __init__(self, file_name):
         self._file_name = file_name
-        self._file_location = None
-
         self._default_output_extension = '.txt'
 
     @property
@@ -17,18 +14,6 @@ class FileWorker:
     @file_name.setter
     def file_name(self, name):
         self._file_name = name
-
-    @property
-    def file_path(self):
-        return self._file_path
-
-    @file_path.setter
-    def file_path(self, path):
-        self._file_path = path
-
-    @property
-    def file_location(self):
-        return str(self.file_path + '/' + self.file_name)
 
     def end_lines_with_comma(self):
         """Read a file as lines, add a comma to the end of each line, save it to a new file.
@@ -50,7 +35,7 @@ class FileWorker:
             new .txt file with a uuid4 name
         """
         new_file_name = str(uuid.uuid4()) + self._default_output_extension
-        with open(self.file_location) as src_file:
+        with open(self.file_name) as src_file:
             with open(new_file_name, 'w') as dest_file:
                 for line in src_file:
                     dest_file.write(str(line.replace(old_word, new_word)))
