c94a6587e8d2d886a04678afa6104b1c41da59c8
M-Farag <mina.farag@icloud.com>
2021-08-13 23:27:42 +0200

    UPD: Setup details

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-	author="Mina Farag",
-	author_email="mina.farag@icloud.com",
-	description="a Data Engineering package",
-	name="EtlWorkers",
-	url="https://github.com/M-Farag/EtlWorkers",
-	version="0.0.4",
-	packages=find_packages(include=["EtlWorkers","EtlWorkers.*"]),
-	install_requires=['uuid'],
+    author="Mina Farag",
+    author_email="mina.farag@icloud.com",
+    description="A Data Engineering package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="EtlWorkers",
+    url="https://github.com/M-Farag/EtlWorkers",
+    version="0.0.5",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(include=["EtlWorkers", "EtlWorkers.*"]),
+    install_requires=['uuid'],
 )
