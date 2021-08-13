90f35f1a7daf08f9cc36977236014c4641e58c1e
M-Farag <mina.farag@icloud.com>
2021-08-13 23:10:13 +0200

    UPD: Using OOP design pattern

--- EtlWorkers/__init__.py
+++ EtlWorkers/__init__.py
@@ -1,3 +1,1 @@
 from . import workers
-from .workers.FileWorker import end_lines_with_comma
-from .workers.FileWorker import replace_a_word_in_each_line 
--- EtlWorkers/workers/FileWorker.py
+++ EtlWorkers/workers/FileWorker.py
@@ -1,49 +1,55 @@
-import time, uuid
-from functools import wraps
+import uuid
 
 
-def timer(func):
-	@wraps(func)
-	def wrapper(*args,**kwargs):
-		start_point = time.time()
-		results = func(*args,**kwargs)
-		run_time = time.time() - start_point
-		print("Func: {}, Runtime is:  {}".format(func.__name__,run_time))
-		return results
-	return wrapper
+class FileWorker:
 
-@timer
-def end_lines_with_comma(file_path:str):
-	"""Read a file as lines, add a comma to the end of each line, save it to a new file.
+    def __init__(self, file_path, file_name):
+        self._file_path = file_path
+        self._file_name = file_name
+        self._file_location = None
 
-	Args:
-		file_path (str): The source file destination and name
-	
-	Returns:
-		new .txt file with a uuid4 name
-	"""
-	new_file_name = str(uuid.uuid4()) + '.txt'
-	with open(file_path) as src_file:
-		with open(new_file_name,'w') as dest_file:
-			for line in src_file:
-				dest_file.write(str(line.strip('\n')+','+'\n'))
+    @property
+    def file_name(self):
+        return self._file_name
 
+    @file_name.setter
+    def file_name(self, name):
+        self._file_name = name
 
-def replace_a_word_in_each_line(file_path:str,old_word:str,new_word:str):
-	""" Read a file as lines, Search and replace a specific word in the whole file, save as new file.
-	
-	Args:
-		file_path (str): The source file destination and name
-		old_word (str): The word you want to look up and replace
-		new_word (str): the word that will replace the old word
-	
-	Returns:
-		new .txt file with a uuid4 name
-	"""
+    @property
+    def file_path(self):
+        return self._file_path
 
-	new_file_name  = str(uuid.uuid4()) + '.txt'
-	with open(file_path) as src_file:
-		with open(new_file_name,'w') as dest_file:
-			for line in src_file:
-				dest_file.write(str(line.replace(old_word,new_word)))
+    @file_path.setter
+    def file_path(self, path):
+        self._file_path = path
 
+    @property
+    def file_location(self):
+        return str(self.file_path + '/' + self.file_name)
+
+    def end_lines_with_comma(self):
+        """Read a file as lines, add a comma to the end of each line, save it to a new file.
+        Returns:
+            new .txt file with a uuid4 name
+        """
+        new_file_name = str(uuid.uuid4()) + '.txt'
+        with open(self.file_name) as src_file:
+            with open(new_file_name, 'w') as dest_file:
+                for line in src_file:
+                    dest_file.write(str(line.strip('\n') + ',' + '\n'))
+
+    def replace_a_word_in_each_line(self, old_word: str, new_word: str):
+        """ Read a file as lines, Search and replace a specific word in the whole file, save as new file.
+        Args:
+            old_word (str): The word you want to look up and replace
+            new_word (str): the word that will replace the old word
+        Returns:
+            new .txt file with a uuid4 name
+        """
+
+        new_file_name = str(uuid.uuid4()) + '.txt'
+        with open(self.file_location) as src_file:
+            with open(new_file_name, 'w') as dest_file:
+                for line in src_file:
+                    dest_file.write(str(line.replace(old_word, new_word)))
